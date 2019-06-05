72e5645880cde637c6977dc363148e2d0d6c85c6
tdowns <travis.downs@gmail.com>
2019-06-05 18:50:41 -0500

    add immintrin.h back to util.cpp for non-PORTABLE builds

--- util.cpp
+++ util.cpp
@@ -13,6 +13,10 @@
 
 #include <sys/mman.h>
 
+#if !UARCH_BENCH_PORTABLE
+#include <immintrin.h>
+#endif
+
 #if defined(__GNUC__) && !defined(__clang__)
 #define GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
 #endif
