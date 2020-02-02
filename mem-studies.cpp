fab351e1aca233aede74994cc96812af332e0773
tdowns <travis.downs@gmail.com>
2020-02-01 21:13:06 -0500

    forwarding tests

--- mem-studies.cpp
+++ mem-studies.cpp
@@ -14,6 +14,15 @@ extern "C" {
 
 bench2_f replay_crossing;
 
+bench2_f fw_write_read;
+bench2_f fw_write_readx;
+bench2_f fw_split_write_read;
+bench2_f fw_split_write_read_chained;
+bench2_f fw_write_split_read;
+bench2_f fw_write_split_read_both;
+bench2_f fw_split_write_split_read;
+
+
 }
 
 template <typename TIMER>
@@ -21,21 +30,26 @@ void register_mem_studies(GroupList& list)
 {
 #if !UARCH_BENCH_PORTABLE
     {
-        // this group of tests isn't directly comparable to the parallel tests since the access pattern is "more random" than the
-        // parallel test, which is strided albeit with a large stride. In particular it's probably worse for the TLB. The result is
-        // that the implied "max MLP" derived by dividing the serial access time by the parallel one is larger than 10 (about 12.5),
-        // which I think is impossible on current Intel. We should make comparable parallel/serial tests that have identical access
-        // patterns.
         std::shared_ptr<BenchmarkGroup> group = std::make_shared<BenchmarkGroup>("studies/replay", "Cacheline crossing loads from fixed-size regions");
         list.push_back(group);
         auto maker = DeltaMaker<TIMER>(group.get(), 10 * 1000);
 
-        // for (int kib = 8; kib <= MAX_SIZE / 1024; kib *= 2) {
-        //     auto& maker = kib > MAX_KIB ? maker_slow : maker_fast;
-        //     MAKE_SERIALO(kib, serial_load_bench, -1);
-        // }
         constexpr size_t size = 10 * 1024 * 1024;
         maker.template make<replay_crossing>("replay-crossing2", "description", 100, []{ return aligned_ptr(64, size, true); });
+    }
+
+    {
+        std::shared_ptr<BenchmarkGroup> group = std::make_shared<BenchmarkGroup>("studies/forwarding", "Forwarding scenarios");
+        list.push_back(group);
+        auto maker = DeltaMaker<TIMER>(group.get(), 10 * 1000);
+
+        #define DEF_FW(name) maker.template make<name> (#name, #name, 100);
+        DEF_FW(fw_write_read);
+        DEF_FW(fw_write_readx);
+        DEF_FW(fw_split_write_read);
+        DEF_FW(fw_split_write_read_chained);
+        DEF_FW(fw_write_split_read);
+        DEF_FW(fw_write_split_read_both);
     }
 
 
--- x86-mem-studies.asm
+++ x86-mem-studies.asm
@@ -40,3 +40,79 @@ align 64
     mov rsp, rbp
     pop_callee_saved
     ret
+
+
+
+
+%macro fw_define_start 1
+define_bench %1
+    push rbp
+    mov  rbp, rsp
+    and rsp, -64
+    sub rsp, 64
+
+    mov eax, 1
+    vpcmpeqd xmm0, xmm0, xmm0
+.top:
+%endmacro
+
+%macro fw_define_end 0
+    dec rdi
+    jnz .top
+
+    mov rsp, rbp
+    pop rbp
+    ret
+%endmacro
+
+fw_define_start fw_write_read
+%rep 100
+    mov [rsp], rax
+    mov rax, [rsp]
+%endrep
+fw_define_end
+
+fw_define_start fw_write_readx
+%rep 100
+    vmovdqa [rsp], xmm0
+    vmovdqa xmm0, [rsp]
+%endrep
+fw_define_end
+
+fw_define_start fw_split_write_read
+%rep 100
+    mov [rsp], rax
+    mov [rsp + 8], rax
+    vmovdqa xmm0, [rsp]
+%endrep
+fw_define_end
+
+fw_define_start fw_split_write_read_chained
+%rep 100
+    mov [rsp], rax
+    mov [rsp + 8], rax
+    vmovdqa xmm0, [rsp]
+    vmovq rax, xmm0
+%endrep
+fw_define_end
+
+
+fw_define_start fw_write_split_read
+%rep 100
+    vmovdqa [rsp], xmm0
+    mov     rax, [rsp]
+    movq    xmm0, rax
+%endrep
+fw_define_end
+
+; same as fw_write_split_read, but reads from both halves of the 16b written value
+; this mostly just adds 1 cycle to the time without revealing any new uarch details
+; on current uarch, but maybe it will false in the future
+fw_define_start fw_write_split_read_both
+%rep 100
+    vmovdqa [rsp], xmm0
+    mov     rax, [rsp]
+    add     rax, [rsp + 8]
+    movq    xmm0, rax
+%endrep
+fw_define_end
